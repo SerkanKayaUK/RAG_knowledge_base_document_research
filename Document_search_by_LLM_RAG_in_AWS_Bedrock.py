8339cfb2e3534f8429f3b16cb29b1fb4c0f1f5e3
Serkan KAYA <70337150+SerkanKayaUK@users.noreply.github.com>
2025-09-22 14:58:21 +0000

    Update on the main code.

--- Document_search_by_LLM_RAG_in_AWS_Bedrock.py
+++ Document_search_by_LLM_RAG_in_AWS_Bedrock.py
@@ -1,1 +1,14 @@
 import boto3
+from langchain_community.embeddings import BedrockEmbeddings
+from langchain_community.llms import Bedrock
+from langchain.prompts.prompt import PromptTemplate
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.vectorstores import FAISS
+from langchain.chains import LLMChain
+
+# Define vectorstore
+global vectorstore_faiss
+
+# Define convenience functions
+def config_llm():
+    
